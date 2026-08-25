84c3a39ed8918a3e41c8460be8fbbbceaa5bf8ba
Daniel <124480168+DanielHzp@users.noreply.github.com>
2026-08-24 20:53:46 -0500

    Typo Global JS custom vars log

--- QPRO_PAGEJS/GLOBALJS_LOGCUSTOMVARIABLES.js
+++ QPRO_PAGEJS/GLOBALJS_LOGCUSTOMVARIABLES.js
@@ -1,11 +1,11 @@
 GLOBALJS_LOGCUSTOMVARIABLES
 
-//Trace API response JSON retrieving custom variable values
+//Trace Browser QPRO API response JSON retrieving custom variable values
 
 function logCustomVariables() {
     var customVars = {};
 
-//I dont use more than variabl 100, but it goes up to 255 custom variabls
+// it goes up to 255 custom variables
     for (var i = 1; i <= 100; i++) {
         customVars["custom" + i] = $survey.surveyResponseJson["custom" + i];
     }
