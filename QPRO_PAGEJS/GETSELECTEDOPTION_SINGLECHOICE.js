d74c70446ffd8c941a90d63441052fd92094ace0
Daniel <124480168+DanielHzp@users.noreply.github.com>
2026-08-25 23:00:08 -0500

    Index comment getselectedOptionIndex

--- QPRO_PAGEJS/GETSELECTEDOPTION_SINGLECHOICE.js
+++ QPRO_PAGEJS/GETSELECTEDOPTION_SINGLECHOICE.js
@@ -3,4 +3,6 @@ GETSELECTEDOPTION_SINGLECHOICE
 
 var selectedAtQ893=$survey.getSelectedOption("Q893");
 
+
 var TotalIncomeHH1=$survey.getSelectedOptionIndex("TotalIncomeHH1");
+//Returns index starting in 1
