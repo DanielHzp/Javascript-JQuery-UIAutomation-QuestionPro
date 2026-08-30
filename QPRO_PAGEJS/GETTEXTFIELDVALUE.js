41b6d30a1c230a01cbb0dd130e9a533958803113
Daniel <124480168+DanielHzp@users.noreply.github.com>
2026-08-30 14:45:14 -0500

    Update get text field value

--- QPRO_PAGEJS/GETTEXTFIELDVALUE.js
+++ QPRO_PAGEJS/GETTEXTFIELDVALUE.js
@@ -1,3 +1,7 @@
 GETTEXTFIELDVALUE
 
-var hhsize = $('.Q846_7 input[type="text"]').val();
+var hhsizeSize = $('.Q846_7 input[type="text"]').val();//If Q846_1 IS TEXT INPUT
+
+var hhsizeInteger = $('.Q846_7 input[type="number"]').val(); // IF Q846_1 IS NUMERIC INPUT
+
+var hhsizeSize = $('.Q846_7 input[type="email"]').val();//If Q846_1 IS EMAIL TEXT TYPE
